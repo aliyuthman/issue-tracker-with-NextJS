a835f5687612ca975079b69f9cc546d4a77c261d
Aliyu Usman <aliyuusman64@gmail.com>
2024-03-17 13:08:06 +0100

    Styling links with classnames package

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -25,9 +26,11 @@ const NavBar = () => {
           <Link
             href={link.href}
             key={link.href}
-            className={`${
-              link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
-            } hover:text-zinc-800 transition-colors`}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
